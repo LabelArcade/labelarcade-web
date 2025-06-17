81ea991fea6b7ff49fdf65059bfafaa0597926ac
Anupam IT <kranupam24@gmail.com>
2025-06-17 15:17:15 +0530

    🐛 Fix CI build: remove unused Navigate import

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,7 +1,6 @@
 // src/components/ProtectedRoute.js
 
 import React from 'react';
-import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = ({ children }) => {
   const token = localStorage.getItem('token');
